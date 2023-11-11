c2ba598a5f58ee8c7bb2c0d8f526c99c8805f76d
delfme <53510751+delfme@users.noreply.github.com>
2023-11-11 10:23:56 +0100

    Add files via upload

---
+++ example/android/app/src/main/kotlin/dev/henryleunghk/flutter_native_text_view_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.henryleunghk.flutter_native_text_view_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,28 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_native_text_view/NativeTextViewPlugin.h>)
+#import <flutter_native_text_view/NativeTextViewPlugin.h>
+#else
+@import flutter_native_text_view;
+#endif
+
+#if __has_include(<simple_platform_view/SimplePlatformViewPlugin.h>)
+#import <simple_platform_view/SimplePlatformViewPlugin.h>
+#else
+@import simple_platform_view;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [NativeTextViewPlugin registerWithRegistrar:[registry registrarForPlugin:@"NativeTextViewPlugin"]];
+  [SimplePlatformViewPlugin registerWithRegistrar:[registry registrarForPlugin:@"SimplePlatformViewPlugin"]];
+}
+
+@end
